3365cf95d69702b728f8b0f5069b99e4ef322c56
Zach Stoebner <zstoebner25@gmail.com>
2021-04-02 15:25:03 -0500

    server boilerplate

--- viz/server.py
+++ viz/server.py
@@ -20,11 +20,51 @@ from sklearn.cluster import KMeans
 app = Flask(__name__)
 CORS(app)
 
-if __name__=='__main__':
-    painting_image_urls = json.load(open('painting_image_urls.json','r'))
-    attribute_names = json.load(open('attribute_names.json','r'))
-    episode_names = json.load(open('episode_names.json','r'))
-    painting_attributes = np.load('painting_attributes.npy')
+timestamps = None	
+edge_graph = None
+#node_graph = None
 
-    app.run()
-#
+def get_timestamps():
+	ts = []
+	for n1,n2,data in edge_graph.edges(data=True):
+		ts += [int(data['timestamp'])]
+#	for n1,n2,data in node_graph.edges(data=True):
+#		ts += [int(data['timestamp'])]
+	ts.sort()
+	print('Number of timestamps: ', len(ts))
+	print('Earliest timestamp: ', ts[0])
+	print('Latest timestamp: ', ts[-1])
+	return ts
+
+def read_json_graph(fname):
+	with open(filename,'r') as f:
+		js_graph = json.load(f)
+	return json_graph.node_link_graph(js_graph)
+
+def get_graph_at_timestamp(timestamp):
+
+	node_bunch = []
+	for n1,n2,data in edge_graph.edges(data=True):
+		node_bunch += [n1,n2] if int(data['timestamp']) <= timestamp else []
+	return nx.subgraph(edge_graph,node_bunch)
+
+@app.route('/all_timestamps',methods=['GET'])
+def all_timestamps():
+	return flask.jsonify(timestamps)
+
+@app.route('/get_timeframe',methods=['GET','POST'])
+def get_timeframe():
+	endtime = request.get_json()
+
+	tf = timestamps[:endtime]
+	subgraph = get_graph_at_timestamp(tf[-1])	
+	graph = json_graph.adjacency_data(subgraph)	
+
+	return flask.jsonify({'timeframe':tf,'graph':graph})
+
+if __name__=='__main__':
+	edge_graph = read_json_graph('static/edge/edge_prediction.json')
+#	node_graph = read_json_graph('static/node/node_classification.json')
+	timestamps = get_timestamps()	
+
+	app.run()
